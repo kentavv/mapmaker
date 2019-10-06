9c29283b35fcaf94303892c7815a90ddfe70a937
Kent VanderVelden <kent.vandervelden@gmail.com>
2019-10-06 14:49:49 -0500

    Removed unused code

--- lib/histogrm.h
+++
@@ -1,7 +0,0 @@
-#ifndef _HISTOGRM_H_
-#define _HISTOGRM_H_
-
-void make_histo(real *dist, int dist_size);
-//void print_histogram(int intervals, char title[10]);
-
-#endif
--- lib/system.h
+++ lib/system.h
@@ -477,8 +477,6 @@ typedef int     bool;
 #define MAYBE	(-1)
 /* rely on the system to define NULL */
 
-#include "histogrm.h"
-
 /* One of the lib_init() routines must be called before ANYTHING else
 is done, or the program will crash in wierd ways! In general, user's
 _init routines should come right after the lib_init() call and should
